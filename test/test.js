c7a49ad15781b673245aa32f23279330ecc07755
zhang.yufeng <zhangyufeng@zbj.com>
2018-05-26 17:17:36 +0800

    fix bug

--- test/test.js
+++ test/test.js
@@ -12,9 +12,11 @@ let jsql = new Jsql({
 });
 
 async function excute() {
-  let data = await jsql.select('article', {id: 2}, ['id']).page(5, 0);
+  let data = await jsql.select('article', ['id']).where({id: 2}).page(5, 0).run();
   console.log(data);
-  data = await jsql.one('article', {id: 2}, ['id', 'title', 'description', 'content', 'create_time']);
+  data = await jsql.one('article', {id: 2}, ['id', 'title', 'description', 'create_time']);
+  console.log(data);
+  data = await jsql.select('article', ['count(*)']).run();
   console.log(data);
 }
 
